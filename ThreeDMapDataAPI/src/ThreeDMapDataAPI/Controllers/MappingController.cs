cfd238d7478d987b92749e8deeb48759ade512d8
Pete D <peted70@hotmail.com>
2017-01-12 19:47:21 +0000

    return model state errors as json

--- ThreeDMapDataAPI/src/ThreeDMapDataAPI/Controllers/MappingController.cs
+++ ThreeDMapDataAPI/src/ThreeDMapDataAPI/Controllers/MappingController.cs
@@ -21,11 +21,13 @@ namespace ThreeDMapDataAPI.Controllers
         // GET api/values
         // http://localhost:8165/api/mapping?maxLon=51.5140574994&maxLat=-0.1145303249&minLon=51.5073134351&minLat=-0.1295164166
         [HttpGet]
-        public async Task<string> Get(GeoRect rect)
+        public async Task<ActionResult> Get(GeoRect rect)
         {
             if (!ModelState.IsValid)
-                return null;
-
+            {
+                return new JsonResult(ModelState);
+            }
+            
             var httpStr = $"http://overpass-api.de/api/interpreter?data=[out:json];(node[\"building\"]({rect.MinLon},{rect.MinLat},{rect.MaxLon},{rect.MaxLat});way[\"building\"]({rect.MinLon},{rect.MinLat},{rect.MaxLon},{rect.MaxLat});relation[\"building\"]({rect.MinLon},{rect.MinLat},{rect.MaxLon},{rect.MaxLat}););out body;>;out skel qt;";
             var http = new HttpClient();
             var tsk = await http.GetAsync(httpStr);
@@ -34,7 +36,7 @@ namespace ThreeDMapDataAPI.Controllers
             Converter.FilesRoot = _he.WebRootPath + Path.DirectorySeparatorChar.ToString();
             var converter = new Converter();
             var geojson = converter.OsmToGeoJSON(strTsk);
-            return geojson;
+            return Content(geojson);
         }
     }
 
